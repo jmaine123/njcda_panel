562bc3a6c35d9a088789d75156e8a0e3252252cf
Jermaine Smikle <jermaine.smikle@aol.com>
2018-09-10 21:57:09 -0400

    style courses show html and started working on seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+admin = User.create({
+  first_name: 'Carl',
+  last_name: 'Smikle',
+  age: 66,
+  email: 'carl@aol.com',
+  password: '123',
+  auth_code: '1234',
+  admin: true
+  })
+
+courses = Course.create(
+  {
+  name: 'React.js',
+  total_hours: '500',
+  description: 'Learning React.js framework at a beginners level'
+}
+)
+
+cohort = Cohorts
