9944653dd5689d0356e461413f8c0a3cef9dca83
Jermaine Smikle <jermaine.smikle@aol.com>
2018-09-13 20:29:37 -0400

    created db:seed to create courses,cohorts,instructors, and students

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@
 require 'faker'
 
 admin = User.create({
-  first_name: 'Carl',
+  first_name: 'Roderick',
   last_name: 'Smikle',
   age: 66,
-  email: 'carl@aol.com',
+  email: 'rod@aol.com',
   password: '123',
   auth_code: '1234',
   admin: true
@@ -28,6 +28,43 @@ courses = Course.create(
 
 cohort = Cohort.create(
   {
-    name: 
+    name: 'React for beginners',
+    max_students: 28,
+    days_per_week: 5,
+    course_id: courses.id,
+    start_date: '2020-01-02 00:00:00',
+    end_date: '2020-04-06 00:00:00'
   }
 )
+
+education = ['High School', 'College', 'Bachelors', 'Masters']
+
+  4.times do
+    student = Student.create(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        age: rand(18..100),
+        email: Faker::Internet.email,
+        phone_number:'201'+ rand.to_s[2..8],
+        education: education[rand(0..3)],
+        course_id: courses.id,
+        cohort_id: cohort.id,
+    )
+    student.email = Faker::Internet.email(student.first_name)
+    student.save
+  end
+
+  instructor = Instructor.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      age: rand(18..100),
+      salary: rand(40000..150000),
+      email: Faker::Internet.email,
+      phone_number:'201'+ rand.to_s[2..8],
+      education: education[rand(0..3)],
+      course_id: courses.id,
+      cohort_id: cohort.id,
+  )
+  instructor.email = Faker::Internet.email(instructor.first_name)
+  instructor.employee_id = instructor.generate_instructor_id
+  instructor.save
