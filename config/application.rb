96a9ef96a5fd06d6e95fd388e4a6fb48092605ae
Jermaine Smikle <jermaine.smikle@aol.com>
2018-09-15 00:31:57 -0400

    Finish installing sentry and finish my db:seeds file. database is populating with students and instructors successfully

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module Panel
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    Raven.configure do |config|
+      config.dsn = 'https://1a44c69acac749bfb7a965da6b020397:2f55c6cddf4d45a29ebd10d664420122@sentry.io/1279935'
+    end
+
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,62 +9,65 @@
 require 'faker'
 
 admin = User.create({
-  first_name: 'Roderick',
+  first_name: 'Jasmine',
   last_name: 'Smikle',
   age: 66,
-  email: 'rod@aol.com',
+  email: 'jasminesmikle@aol.com',
   password: '123',
   auth_code: '1234',
   admin: true
   })
 
-courses = Course.create(
-  {
-  name: 'React.js',
-  total_hours: '500',
-  description: 'Learning React.js framework at a beginners level'
-}
-)
+12.times do
 
-cohort = Cohort.create(
-  {
-    name: 'React for beginners',
-    max_students: 28,
-    days_per_week: 5,
-    course_id: courses.id,
-    start_date: '2020-01-02 00:00:00',
-    end_date: '2020-04-06 00:00:00'
-  }
-)
-
-education = ['High School', 'College', 'Bachelors', 'Masters']
-
-  4.times do
-    student = Student.create(
-        first_name: Faker::Name.first_name,
-        last_name: Faker::Name.last_name,
-        age: rand(18..100),
-        email: Faker::Internet.email,
-        phone_number:'201'+ rand.to_s[2..8],
-        education: education[rand(0..3)],
-        course_id: courses.id,
-        cohort_id: cohort.id,
+    course = Course.create(
+      {
+      name: Faker::ProgrammingLanguage.name,
+      total_hours: rand(40..100),
+      description: 'Beginner level course'
+    }
     )
-    student.email = Faker::Internet.email(student.first_name)
-    student.save
-  end
 
-  instructor = Instructor.create(
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      age: rand(18..100),
-      salary: rand(40000..150000),
-      email: Faker::Internet.email,
-      phone_number:'201'+ rand.to_s[2..8],
-      education: education[rand(0..3)],
-      course_id: courses.id,
-      cohort_id: cohort.id,
-  )
-  instructor.email = Faker::Internet.email(instructor.first_name)
-  instructor.employee_id = instructor.generate_instructor_id
-  instructor.save
+    cohort = Cohort.create(
+      {
+        name: "#{course.name} Summer 101 ",
+        max_students: rand(20..50),
+        days_per_week: rand(1..7),
+        course_id: course.id,
+        start_date: '2020-01-06 00:00:00',
+        end_date: '2020-04-08 00:00:00'
+      }
+    )
+
+    education = ['High School', 'College', 'Bachelors', 'Masters']
+
+      20.times do
+        student = Student.create(
+            first_name: Faker::Name.first_name,
+            last_name: Faker::Name.last_name,
+            age: rand(18..100),
+            email: Faker::Internet.email,
+            phone_number:'201'+ rand.to_s[2..8],
+            education: education[rand(0..3)],
+            course_id: course.id,
+            cohort_id: cohort.id,
+        )
+        student.email = Faker::Internet.email(student.first_name)
+        student.save
+      end
+
+      instructor = Instructor.create(
+          first_name: Faker::Name.first_name,
+          last_name: Faker::Name.last_name,
+          age: rand(18..100),
+          salary: rand(40000..150000),
+          email: Faker::Internet.email,
+          phone_number:'201'+ rand.to_s[2..8],
+          education: education[rand(0..3)],
+          course_id: course.id,
+          cohort_id: cohort.id,
+      )
+      instructor.email = Faker::Internet.email(instructor.first_name)
+      instructor.employee_id = instructor.generate_instructor_id
+      instructor.save
+end
