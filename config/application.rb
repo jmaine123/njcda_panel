1b4bcf99f00fbf13fdc83ca0751995da4e5f502e
Jermaine Smikle <jermaine.smikle@aol.com>
2018-09-24 16:17:12 -0400

    add environment var to heroku

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Panel
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     Raven.configure do |config|
-      config.dsn = 'https://1a44c69acac749bfb7a965da6b020397:2f55c6cddf4d45a29ebd10d664420122@sentry.io/1279935'
+      config.dsn = ENV['SENTRY_ENV_VAR']
     end
 
   end
